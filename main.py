fc38ce1936ae3f36df48f847c4b1a16d1d12b655
Walter <zikavaclav05@gmail.com>
2022-06-14 19:15:16 +0200

    trying

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-#app.config["SECRET_KEY"] = "WalterIsCull"
+app.config["SECRET_KEY"] = "WalterIsCull"
 socketio = SocketIO(app, cors_allowed_origins="*")
 usernames = {}
 
@@ -38,4 +38,5 @@ def handle_connection():
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host='0.0.0.0', port=5004)
+    print("HUH")
+    socketio.run(app)
