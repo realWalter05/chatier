7c16ca00840636d50610dd29eff23e542d306295
Walter <zikavaclav05@gmail.com>
2022-06-14 20:15:24 +0200

    lol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from flask.ext.socketio import SocketIO
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "WalterIsCull"
