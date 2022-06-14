2af291cd2b5af139a54060ebbb97e193b89fbf3d
Walter <zikavaclav05@gmail.com>
2022-06-14 23:59:40 +0200

    v05

--- static/main.js
+++ static/main.js
@@ -2,10 +2,21 @@ $(document).ready(function() {
 console.log("Jquery works...");
 
 $("#connect-btn").on("click", function () {
-    const socket = io.connect("/")
+    const socket = io.connect("https://chatier.herokuapp.com/",
+    {
+        'transports': ['polling'],
+        'autoConnect': true,
+        'pingInterval': 25000,
+        'pingTimeout': 180000,
+        'reconnection': true,
+        'reconnectionDelay': 1000,
+        'reconnectionDelayMax': 5000,
+        'reconnectionAttempts': 5,
+		'secure': true
+    });
 
     socket.on("connect", function () {
-        let username = $("#username-input").val()
+        let username = $("#username-input").val();
         socket.send(["set_username", username]);
     });
 
@@ -18,19 +29,19 @@ $("#connect-btn").on("click", function () {
                     $("#online").append("<li>" + msg[1][i] + "</li>");
                 }
             } else {
-                let sender = msg[0]
-                let message = msg[1]
-                let time = msg[2]
+                let sender = msg[0];
+                let message = msg[1];
+                let time = msg[2];
 
                 if (document.querySelectorAll("#"+sender).length == 0) {
                     // Create conversation div
                     let conversationDiv = document.createElement("section");
-                    conversationDiv.setAttribute("id", sender)
-                    conversationDiv.setAttribute("class", "w-100 p-5 bg-light overflow-auto")
+                    conversationDiv.setAttribute("id", sender);
+                    conversationDiv.setAttribute("class", "w-100 p-5 bg-light overflow-auto");
                     
                     let title = document.createElement("h3");
                     title.innerText = sender;
-                    conversationDiv.append(title)
+                    conversationDiv.append(title);
                     $("#result").append(conversationDiv);
                 }
 
@@ -47,19 +58,19 @@ $("#connect-btn").on("click", function () {
         if (document.querySelectorAll("#"+msg[0]).length == 0) {
             // Create conversation div
             let conversationDiv = document.createElement("section");
-            conversationDiv.setAttribute("id", msg[0])
-            conversationDiv.setAttribute("class", "w-100 p-5 bg-light overflow-auto")
+            conversationDiv.setAttribute("id", msg[0]);
+            conversationDiv.setAttribute("class", "w-100 p-5 bg-light overflow-auto");
             
             let title = document.createElement("h3");
             title.innerText = msg[0];
-            conversationDiv.append(title)
+            conversationDiv.append(title);
             $("#result").append(conversationDiv);
         }
 
         let hour = new Date().getHours();
         let minute = new Date().getMinutes();
         let second = new Date().getSeconds();
-        let time = hour + ":" + minute + ":" + second
+        let time = hour + ":" + minute + ":" + second;
         $("#"+msg[0]).append(time + " " + $("#username-input").val() + ": " + msg[1] + "<br/>");        
     });
 });
