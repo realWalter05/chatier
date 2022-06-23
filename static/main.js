94c485d275264df3984ae7216d1aa77c97588ca7
Walter <zikavaclav05@gmail.com>
2022-06-23 19:42:50 +0200

    slow_speed_fix

--- static/main.js
+++ static/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     
     $("#connect-btn").on("click", function () {
         // const socket = io.connect("");
-        const socket = io.connect("/");
+        const socket = io.connect();
     
         socket.on("connect", function () {
             let username = $("#username-input").val();
