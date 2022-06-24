c6c8ea3a4e5a5db1972e6055f672b4ae8438693f
realWalter05 <78427254+realWalter05@users.noreply.github.com>
2022-06-24 12:48:57 +0200

    update

--- static/main.js
+++ static/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     
     $("#connect-btn").on("click", function () {
         // const socket = io.connect("");
-        const socket = io.connect();
+        const socket = io.connect("http://35.198.78.211");
     
         socket.on("connect", function () {
             let username = $("#username-input").val();
--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,4 @@ from main import app, socketio
 
 if __name__ == "__main__":
     print("Chatier intiated.")
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=5000)
