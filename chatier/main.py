19badfa28c656e56d162e7d6603182eec933b206
Walter <zikavaclav05@gmail.com>
2022-07-01 20:24:57 +0200

    0.7

--- chatier/main.py
+++ chatier/main.py
@@ -1,49 +1,63 @@
 from flask import Flask, request, render_template
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
 from datetime import datetime
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "WalterIsCull"
 socketio = SocketIO(app, cors_allowed_origins="*")
-usernames = {}
+clients = {}
+
+def set_get_msg(content, sender, receiver):
+    return {
+        "content" : content,
+        "sender" : sender,
+        "receiver" : receiver
+    }
 
 @app.route('/')
 def index():
     print("Indexing")
     return render_template('index.html')
-    
-@app.route('/design')
-def design():
-    return render_template('index_new.html')
 
+@socketio.on("client_registration")
+def register_client(username):
+    print(f"{username} wants to register with an id {request.sid}")
+    client_id = request.sid
+    
+    if username in clients.values():
+        emit("username_taken", to=client_id)
+        return
+    clients[client_id] = username
 
 @socketio.on("message")
 def handle_message(msg):
-    if msg[0] == "set_username":
-        # Setting username
-        print(msg[1])
-        usernames[request.sid] = msg[1]
-        send(["usernames", msg[1]], broadcast=True)
-
-    elif msg[0] == "message":
-        if msg[1][0] in list(usernames.values()):
-            print("Msg sent: " + str(msg))
-            receiver = list(usernames.keys())[list(usernames.values()).index(msg[1][0])]
-            message = [usernames[request.sid], msg[1][1], datetime.now().strftime("%H:%M:%S")]
-            send(message, to=receiver)
+    client_id = request.sid
+    if client_id in clients.keys():
+        print("Msg sent: " + str(msg))
+        # Getting the receiver sid
+        if not msg["receiver"] in clients.values():
+            print("Receiver is not online")
+            return
+        receiver = list(clients.keys())[list(clients.values()).index(msg["receiver"])]
+        msg = {
+            "sender" : clients[client_id],
+            "content" : msg["content"]
+        }
+        send(msg, to=receiver)
 
 @socketio.on("connect")
 def handle_connection():
     print("Player connects:")
     # Save username to dictionary
-    send(["usernames_full", list(usernames.values())], to=request.sid)
-    usernames[request.sid] = ""
+   # send(["clients_full", list(clients.values())], broadcast=True)
+    clients[request.sid] = ""
 
 @socketio.on("disconnect")
 def handle_connection():
     print("Player disconnects")
     # Save username to dictionary
-    print(usernames)
-    usernames.pop(request.sid)
-    print(usernames)
-    send(["usernames_full", list(usernames.values())], broadcast=True)
+    print(clients)
+    clients.pop(request.sid)
+    clients.pop(request.sid)
+    print(clients)
+   # send(["clients_full", list(clients.values())], broadcast=True)
--- chatier/static/main.js
+++ chatier/static/main.js
@@ -1,73 +1,173 @@
-$(document).ready(function() {
-    console.log("Jquery works...");
-    
-    $("#connect-btn").on("click", function () {
-        const socket = io.connect("/");
-    
-        socket.on("connect", function () {
-            let username = $("#username-input").val();
-            socket.send(["set_username", username]);
-        });
-    
-        socket.on("message", function (msg) {
-            if (Array.isArray(msg)) {
-                if (msg[0] == "usernames") {
-                    $("#online").append("<li>" + msg[1] + "</li>");
-                } else if (msg[0] == "usernames_full") {
-                    // Deleting previous li
-                    let previousLis = document.querySelectorAll("li");
-                    previousLis.forEach(li => {
-                        li.remove();
-                    });
 
-                    for(let i = 0; i < msg[1].length; i++) {
-                        $("#online").append("<li>" + msg[1][i] + "</li>");
-                    }
-                } else {
-                    let sender = msg[0];
-                    let message = msg[1];
-                    let time = msg[2];
-    
-                    if (document.querySelectorAll("#"+sender).length == 0) {
-                        // Create conversation div
-                        let conversationDiv = document.createElement("section");
-                        conversationDiv.setAttribute("id", sender);
-                        conversationDiv.setAttribute("class", "w-100 p-5 bg-light overflow-auto");
-    
-                        let title = document.createElement("h3");
-                        title.innerText = sender;
-                        conversationDiv.append(title);
-                        $("#result").append(conversationDiv);
-                    }
-    
-                    $("#"+sender).append(time + " " + sender + ": " + message + "<br/>");
-                }
-            }
+var chats = {}
+$(document).ready(function() {
+    // When user opens the page
+    console.log("Jquery works...?");
+
+    $("#register-btn").on("focus", function () {
+        // Selecting username
+        if ($("#register-btn").text() == "Connect") {
+            $("#register-btn").text("Write your username!");    
+        }
+
+        $("#register-btn").on("keydown", function (e) {
+            // Handling no letter keys
+            if (e.key == "Backspace") {
+                $("#register-btn").text($("#register-btn").text().slice(0, -1)); 
+            } else if (e.key == "Enter") {
+                $("#register-btn").blur();
+            }                    
         });
+
+        $("#register-btn").on("keypress", function (e) {
+            // Handling text keypresses
+            if ($("#register-btn").text() == "Write your username!" || $("#register-btn").text() == "Username taken!" ) {
+                $("#register-btn").text("");
+            }
+            $("#register-btn").append(e.key); 
+        });
+    });
     
+    $("#register-btn").on("focusout", function () {
+        // User selected username
+        $("#register-btn").off("keydown");
+        $("#register-btn").off("keypress");
+
+        // Connecting to chat
+        const socket = io.connect("/");
+        let username = $("#register-btn").text();
+        socket.emit("client_registration", username)
+
+        socket.on("username_taken", function () {
+            // Username was taken
+            socket.disconnect()
+            $("#register-btn").text("Username taken!");
+            $("#register-btn").focus();
+        });
+
+
+        socket.on("message", function (msg) {
+            console.log(chats);
+            // Message received
+            let sender = msg["sender"]
+            let content = msg["content"]
+            let msg_dict = {"receiver-li" : msg["content"]}
+
+            if (!(sender in chats)) {
+                // Chat with this msg sender doesn't exist yet, so we create it
+                chats[sender] = []                
+                $(`
+                    <a onclick="ChangeChat(this);" class="list-group-item list-group-item-action w-100 px-4 py-4" aria-current="true">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <strong class="mb-1">` + sender + `</strong><small class="text-success">New message</small>
+                        </div>
+                        <div class="col mb-1 small">
+                            ` + content + `
+                        </div>
+                    </a>`).insertBefore('#new-msg-btn');
+            }
+
+            // Adding a new message
+            chats[sender].push(msg_dict)
+            
+            if ($("#selected-chat").find("strong").text() == sender) {
+                // New message arrived from sender we have open chat with
+                $('<li/>',{
+                    text: msg["content"],
+                    class: "receiver-li",
+                }).appendTo('#msg-space');
+            }
+            // Updating chat options
+            let newMessageDiv = $("strong:contains('" + sender + "')").parent().parent();
+            $(newMessageDiv).find("small").text("New message").addClass("text-success");
+            $(newMessageDiv).children().last().text(content);
+        });
+        
         $("#send-msg-btn").on("click", function () {
-            let msg = [$("#receiver-input").val(), $("#msg-input").val()];
-            console.log(msg);
-            socket.send(["message", msg]);
-    
-            if (document.querySelectorAll("#"+msg[0]).length == 0) {
-                // Create conversation div
-                let conversationDiv = document.createElement("section");
-                conversationDiv.setAttribute("id", msg[0]);
-                conversationDiv.setAttribute("class", "w-100 p-5 bg-light overflow-auto");
-    
-                let title = document.createElement("h3");
-                title.innerText = msg[0];
-                conversationDiv.append(title);
-                $("#result").append(conversationDiv);
+            // Sending a message
+            let receiver = $("#selected-chat").find("strong").text();
+            let content = $("#msg-input").val();
+            let msg = {
+                "receiver" : receiver,
+                "content" : content,
             }
-    
-            let hour = new Date().getHours();
-            let minute = new Date().getMinutes();
-            let second = new Date().getSeconds();
-            let time = hour + ":" + minute + ":" + second;
-            $("#"+msg[0]).append(time + " " + $("#username-input").val() + ": " + msg[1] + "<br/>");
+            let msg_dict = {"sender-li" : content};
+            chats[receiver].push(msg_dict)
+            $('<li/>',{
+                text: content,
+                class: "sender-li",
+            }).appendTo('#msg-space');
+            $('#msg-space').scrollTop($('#msg-space').get(0).scrollHeight);
+            socket.send(msg);
+
+            // Reset msg input
+            $("#msg-input").val("");
         });
     });
-    
+
+    $("#new-msg-btn").on("focus", function () {
+        // Creating a new chat
+        $("#new-msg-btn").text("To whom?")
+
+        $("#new-msg-btn").on("keydown", function (e) {
+            // Handling no letter keys
+            if (e.key == "Backspace") {
+                $("#new-msg-btn").text($("#new-msg-btn").text().slice(0, -1)); 
+            } else if (e.key == "Enter") {
+                $("#new-msg-btn").blur();
+            }                    
+        });
+
+        $("#new-msg-btn").on("keypress", function (e) {
+            // Handling text keypresses
+            if ($("#new-msg-btn").text() == "New message" || $("#new-msg-btn").text() == "To whom?") {
+                $("#new-msg-btn").text("");
+            }
+            $("#new-msg-btn").append(e.key); 
+        });      
     });
+
+    $("#new-msg-btn").on("focusout", function () {
+        // User selected chat receiver
+        $("#new-msg-btn").off("keydown");
+        $("#new-msg-btn").off("keypress");
+        let sender = $("#new-msg-btn").text() 
+        chats[sender] = []
+
+        $(`
+        <a onclick="ChangeChat(this);" class="list-group-item list-group-item-action w-100 px-4 py-4" aria-current="true">
+            <div class="d-flex align-items-center justify-content-between">
+                <strong class="mb-1">` + sender + `</strong><small>No messages</small>
+            </div>
+            <div class="col mb-1 small">
+                Write him something :)
+            </div>
+        </a>`).insertBefore('#new-msg-btn').click();
+        $("#new-msg-btn").text("New message")
+    });
+
+    $("#msg-input").on("keypress", function (e) {
+        if ((e.keyCode || e.which) == 13) {
+            $("#send-msg-btn").click();
+            return false;
+          }        
+    });
+});
+
+function ChangeChat(selectedChat) {
+    // Changing chats
+    $("#msg-space").empty();
+    $("#selected-chat").removeAttr('id');
+    $(selectedChat).attr("id", "selected-chat");
+
+    let sender = $(selectedChat).find("strong").text();
+    
+    $.each(chats[sender], function(index, chatDict) {
+        for (let key in chatDict) {
+            $('<li/>',{
+                text: chatDict[key],
+                class: key,
+            }).appendTo('#msg-space');
+        }
+    });
+}
